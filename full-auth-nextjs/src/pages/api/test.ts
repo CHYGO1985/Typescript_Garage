cb1851597b4b2edf0ad8d7876b9c2652bf6124f3
CHYGO1985 <jingjiejiang85@gmail.com>
2023-05-18 21:53:05 +0800

    add mongo connect and test success

---
+++ full-auth-nextjs/src/pages/api/test.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import getMongodbInstance from '@/utils/connect-mongodb'
+
+type Data = {
+  name: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  await getMongodbInstance()
+}
