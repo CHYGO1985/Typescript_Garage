75f6a5fcd3ff8fd9e4bbc0d23d1b2d88abf6f32f
CHYGO1985 <jingjiejiang85@gmail.com>
2023-05-19 22:21:41 +0800

    add github signin and test success

--- full-auth-nextjs/src/pages/index.tsx
+++ full-auth-nextjs/src/pages/index.tsx
@@ -1,7 +1,22 @@
+import { useSession, signIn, signOut, getSession } from 'next-auth/react'
+import { NextPageContext } from 'next'
+
 export default function Home() {
+  const { data: session } = useSession()
   return (
     <>
       <h1 className='text-red-700 text-4xl bg-lime-300'>full auth</h1>
+      <button onClick={() => signOut()}>Sign out</button>
     </>
   )
+}
+
+// for getting session info
+export async function getServerSideProps(ctx: NextPageContext) {
+  const session = await getSession(ctx)
+  return {
+    props: {
+      session,
+    },
+  }
 }
