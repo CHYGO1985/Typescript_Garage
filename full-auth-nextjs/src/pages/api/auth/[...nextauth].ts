efc0f4c3e5f20578411c294d662d42ed3439309e
CHYGO1985 <jingjiejiang85@gmail.com>
2023-05-20 22:19:53 +0800

    Add auth0 and twitter

--- full-auth-nextjs/src/pages/api/auth/[...nextauth].ts
+++ full-auth-nextjs/src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import GithubProvider from 'next-auth/providers/github'
+import Auth0Provider from 'next-auth/providers/auth0'
+import TwitterProvider from 'next-auth/providers/twitter'
 
 export default NextAuth({
   secret: process.env.SECRET,
@@ -14,6 +16,16 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    // Auth0
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID as string,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+      issuer: process.env.AUTH0_ISSUER as string,
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID as string,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
     }),
   ],
 })
--- full-auth-nextjs/src/pages/index.tsx
+++ full-auth-nextjs/src/pages/index.tsx
@@ -5,8 +5,23 @@ export default function Home() {
   const { data: session } = useSession()
   return (
     <>
-      <h1 className='text-red-700 text-4xl bg-lime-300'>full auth</h1>
-      <button onClick={() => signOut()}>Sign out</button>
+      <h1 className='text-red-700 text-4xl bg-lime-300'>
+        {session?.user?.name}
+      </h1>
+      <img
+        src={session?.user?.image!}
+        alt='user image'
+        className='w-32 h-32 rounded-full'
+      />
+      {session ? (
+        <button className='bg-blue-400' onClick={() => signOut()}>
+          Sign out
+        </button>
+      ) : (
+        <button className='bg-blue-400' onClick={() => signIn()}>
+          Sign in
+        </button>
+      )}
     </>
   )
 }
