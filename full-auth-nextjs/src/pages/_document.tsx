65ed09b901a30eb356ac59c6d683705841876c45
CHYGO1985 <jingjiejiang85@gmail.com>
2023-05-23 16:03:17 +0800

    add homepage css and html

--- full-auth-nextjs/src/pages/_document.tsx
+++ full-auth-nextjs/src/pages/_document.tsx
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <title>Authentication -- JJ</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
