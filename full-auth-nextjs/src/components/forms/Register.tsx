ab035fa205ffbd1219d265bbc61119cd9796e0ec
CHYGO1985 <jingjiejiang85@gmail.com>
2023-05-24 21:13:16 +0800

    add first name input box and zod checker

--- full-auth-nextjs/src/components/forms/Register.tsx
+++ full-auth-nextjs/src/components/forms/Register.tsx
@@ -1,20 +1,48 @@
 import * as React from 'react'
 import Input from '../inputs/Inputs'
 import { CiUser } from 'react-icons/ci'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 interface IRegisterFormProps {}
 
+const FormSchema = z.object({
+  first_name: z
+    .string()
+    .min(2, 'First name must be at least 2 characters')
+    .max(32, 'First name must be less than 32 characters')
+    .regex(new RegExp('^[a-zA-z]+$'), 'No special characters allowed.'),
+})
+
+type FormSchemaType = z.infer<typeof FormSchema>
+
 const RegisterForm: React.FunctionComponent<IRegisterFormProps> = (props) => {
+  const {
+    register,
+    handleSubmit,
+    watch, // watch the value
+    formState: { errors, isSubmitting },
+  } = useForm<FormSchemaType>({
+    resolver: zodResolver(FormSchema),
+  })
+
+  const onSubmit = (data: any) => console.log(data)
+
   return (
-    <form className='my-8 text-sm'>
+    <form className='my-8 text-sm' onSubmit={handleSubmit(onSubmit)}>
       <div className='gap-2 md:flex'>
         <Input
-          name='fist_name'
+          name='first_name'
           label='First name'
           type='text'
           icon={<CiUser />}
           placeholder='example'
-        ></Input>
+          register={register}
+          error={errors?.first_name?.message}
+          disabled={isSubmitting}
+        />
+        <button type='submit'>Submit</button>
       </div>
     </form>
   )
